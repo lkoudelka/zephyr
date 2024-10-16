3137cdf925ae2c023c801e2aaad1f1fd7c1dfdf7
Lubos Koudelka <lubos.koudelka@st.com>
2024-10-16 10:40:40 +0200

    simplification of circular DMA example
    Signed-off-by:Lubos Koudelka <lubos.koudelka@st.com>
    
    Signed-off-by:Lubos Koudelka <lubos.koudelka@st.com>

--- samples/drivers/uart/uart_wba_dma/src/main.c
+++ samples/drivers/uart/uart_wba_dma/src/main.c
@@ -13,10 +13,9 @@
 /* change this to any other UART peripheral if desired */
 #define UART_DEVICE_NODE DT_CHOSEN(zephyr_shell_uart)
 
-#define MSG_SIZE 32
+#define MSG_SIZE 256
 
-/* queue to store up to 10 messages (aligned to 4-byte boundary) */
-K_MSGQ_DEFINE(uart_msgq, MSG_SIZE, 10, 4);
+
 
 static const struct device *const uart_dev = DEVICE_DT_GET(UART_DEVICE_NODE);
 
@@ -30,32 +29,8 @@ static int rx_buf_pos;
  */
 void serial_cb(const struct device *dev, void *user_data)
 {
-	uint8_t c;
+	print_uart("UART RX interrupt");
 
-	if (!uart_irq_update(uart_dev)) {
-		return;
-	}
-
-	if (!uart_irq_rx_ready(uart_dev)) {
-		return;
-	}
-
-	// /* read until FIFO empty */
-	// while (uart_fifo_read(uart_dev, &c, 1) == 1) {
-	// 	if ((c == '\n' || c == '\r') && rx_buf_pos > 0) {
-	// 		/* terminate string */
-	// 		rx_buf[rx_buf_pos] = '\0';
-
-	// 		/* if queue is full, message is silently dropped */
-	// 		k_msgq_put(&uart_msgq, &rx_buf, K_NO_WAIT);
-
-	// 		/* reset the buffer (it was copied to the msgq) */
-	// 		rx_buf_pos = 0;
-	// 	} else if (rx_buf_pos < (sizeof(rx_buf) - 1)) {
-	// 		rx_buf[rx_buf_pos++] = c;
-	// 	}
-	// 	/* else: characters beyond buffer size are dropped */
-	// }
 }
 
 /*
@@ -111,10 +86,6 @@ int main(void)
 	print_uart("Tell me something and press enter:\r\n");
 
 	/* indefinitely wait for input from the user */
-	while (k_msgq_get(&uart_msgq, &tx_buf, K_FOREVER) == 0) {
-		print_uart("Echo: ");
-		print_uart(tx_buf);
-		print_uart("\r\n");
-	}
+	while (1){}
 	return 0;
 }
